4ffa76395afd9ae07958a36987762e56fc55b126
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-08-19 08:51:56 +1000

    undo

--- src/helper/discord-bot.ts
+++ src/helper/discord-bot.ts
@@ -1,2 +1,2 @@
 
-export const INVITE_LINK = "https://discord.com/oauth2/authorize?client_id=1250478476654542988&permissions=66560&integration_type=0&scope=bot+applications.commands+channels:read"
+export const INVITE_LINK = 'https://discord.com/oauth2/authorize?client_id=1250478476654542988&permissions=66560&integration_type=0&scope=bot+applications.commands+channels:read'
