c5bd0fcf3fead993b5afb544f201a931fd7fb3bb
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-08-19 08:48:04 +1000

    refactor wallet team message processing into a file on its own

--- src/service/discord-bot/index.ts
+++ src/service/discord-bot/index.ts
@@ -1,46 +1,26 @@
-import { ChannelType, Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import { WebClient } from "@slack/web-api";
 import { Logger } from "pino";
 
-import { SLACK_POST_CHANNEL, WALLET_KEYWORDS } from "../../helper/discord-bot";
+import { walletTeamWatchMessages } from "./wallet-team";
 
 export const watchMessages = async (discordToken: string, slackToken: string, logger: Logger) => {
   const discordClient = new Client({
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent
-    ]
-  })
-  const slackClient = new WebClient(slackToken)
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+  const slackClient = new WebClient(slackToken);
 
-  discordClient.once(Events.ClientReady, readyClient => {
-    logger.info(`Logged in as ${readyClient.user.tag}`)
-  })
-
-  discordClient.on('messageCreate', async (message) => {
-    // Ignore messages from bots
-    if (message.author.bot) return;
-  
-    const messageContent = message.content.toLowerCase();
-    const containsKeyword = WALLET_KEYWORDS.some(keyword => messageContent.includes(keyword.toLowerCase()));
-
-    if (containsKeyword) {
-      const channel = await discordClient.channels.fetch(message.channelId);
-      const channelName = channel && channel.type === ChannelType.GuildText ? channel.name : message.channelId
-      const res = await slackClient.chat.postMessage({
-        channel: SLACK_POST_CHANNEL,
-        text: `channel: *${channelName}*, from *${message.author.username}*:
-${message.content}
-        `
-      });
-
-      if (!res.ok) {
-        logger.error('Failed to post to slack')
-        logger.error(res.errors)
-      }
-    }
+  discordClient.once(Events.ClientReady, (readyClient) => {
+    logger.info(`Logged in as ${readyClient.user.tag}`);
   });
 
-  discordClient.login(discordToken)
-}
+  discordClient.on("messageCreate", async (message) => {
+    return Promise.all([walletTeamWatchMessages(message, discordClient, slackClient, logger)]);
+  });
+
+  discordClient.login(discordToken);
+};
